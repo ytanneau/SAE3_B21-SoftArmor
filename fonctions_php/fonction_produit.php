598d57e51ceb418cafd3e7ca19f293723d30759c
Edouard Le Lay <edouardlelay@dn042109.etu.univ-rennes1.fr>
2025-11-05 14:42:15 +0100

    correction produit

--- fonctions_php/fonction_produit.php
+++ fonctions_php/fonction_produit.php
@@ -6,13 +6,13 @@
         }
         // code de iwan pour calculer et afficher les moyennes d'un produit en fonction de sa moyenne
         for ($i =1; $i <= floor($moyenne); $i++){
-            ?> <img src="../html/images/etoile_pleine.svg" alt="étoile pleine"><?php
+            ?> <img src="images/etoile_pleine.svg" alt="étoile pleine"><?php
         }
         if(fmod(floor($moyenne*2),2)){
-            ?> <img src="../html/images/etoile_demi.svg" alt="étoile à moitié pleine"> <?php 
+            ?> <img src="images/etoile_demi.svg" alt="étoile à moitié pleine"> <?php 
         }
         for ($i =5; $i > round($moyenne); $i--){
-            ?> <img src="../html/images/etoile_vide.svg" alt="étoile vide"><?php
+            ?> <img src="images/etoile_vide.svg" alt="étoile vide"><?php
         }
     }
 ?>
