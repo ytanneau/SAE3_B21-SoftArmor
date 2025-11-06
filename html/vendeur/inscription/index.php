d1e067755d7e2cf8019c9fa3314e81f78f8f83ae
ytanneau <tannyann29@gmail.com>
2025-11-06 14:44:53 +0100

    getenv fix

--- html/vendeur/inscription/index.php
+++ html/vendeur/inscription/index.php
@@ -3,7 +3,7 @@
     if ($_POST != null){
         //echo "présence d'un post";
         print_r($_ENV);
-        $fichier = $getenv('HOME_GIT') . '/fonction_compte.php';
+        $fichier = getenv('HOME_GIT') . '/fonction_compte.php';
         if (file_exists($fichier)) {
             require_once $fichier;
             $res = create_profile_vendeur($_POST['raisonSocial'], $_POST['numSiret'], $_POST['numCobrec'], $_POST['email'], $_POST['adresse'], $_POST['codePostal'], $_POST['mdp'], $_POST['mdpc']);
