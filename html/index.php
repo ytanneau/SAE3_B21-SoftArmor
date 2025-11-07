0c9f6b4aa0a99b5c0ceb9307bf853ee8c6ee933a
Edouard LE LAY <89097238+LELAYEdouard@users.noreply.github.com>
2025-11-07 13:51:20 +0100

    FINAL CHEMIN

--- html/index.php
+++ html/index.php
@@ -3,7 +3,7 @@ define('HOME_GIT', "../" );
 session_start();
 
 require_once (HOME_GIT . '.config.php');
-require_once (HOME_GIT . 'fonctions_php/fonction_compte.php');
+require_once (HOME_GIT . 'fonctions_php/fonction_produit.php');
 
 // requete pour recuperer le nom public, le prix , la moyenne des notes et les informations de l'image de chaque produit
 $query= "SELECT nom_public,prix,url_image,alt,_image.titre,note_moy AS moyenne FROM produit_visible INNER JOIN _images_produit ON produit_visible.id_produit = _images_produit.id_produit INNER JOIN _image ON _images_produit.id_image_principale = _image.id_image INNER JOIN produit_note ON produit_note.id_produit = produit_visible.id_produit WHERE produit_note.id_produit = produit_visible.id_produit;";
