d425ce835c5e25f5f560b37cfeaa0251f2c687c5
mathislqs <mathisloquais@gmail.com>
2025-11-07 17:05:10 +0100

    Update index.php

--- html/vendeur/stock/index.php
+++ html/vendeur/stock/index.php
@@ -9,25 +9,8 @@
 
 <?php
 require_once '../../../.config.php';
-$sql = 'select nom_stock from _produit where id_vendeur = :id_vendeur';
-$nom_stock = initialize($sql);
-
-$sql2 = 'select quantite from _produit where id_vendeur = :id_vendeur';
-$quantite = initialize($sql2);
-
-
-if ($nom_stock->execute()) {
-    if($quantite->execute()){
-        if ($quantite->rowCount() > 0) {
-            atrapperNom($nom_stock, $quantite);
-        } else {
-            echo "Vous n'avez pas de produit.";
-        }
-
-        } else {
-            echo "Il y a eu un problème. Veuillez réessayer plus tard.";
-        }
-}
+$sql = 'select nom_stock, quantite from _produit where id_vendeur = :id_vendeur';
+$stmt = initialize($sql);
 
 unset($quantite);
 
@@ -38,16 +21,25 @@ function initialize($sql){
     if ($stmt = $pdo->prepare($sql)) {
         $stmt->bindParam(":id_vendeur", $compte);
     }
+    if ($stmt->execute()) {
+        if ($stmt->rowCount() > 0) {
+            atrapperNom($stmt);
+        } else {
+            echo "Vous n'avez pas de produit.";
+        }
+
+        } else {
+            echo "Il y a eu un problème. Veuillez réessayer plus tard.";
+        }
     return $stmt;
 } 
 
 
 
 
-function atrapperNom($nom_stock, $quantite){
-    $row = $nom_stock->fetch(PDO::FETCH_ASSOC);
-    $row2 = $quantite->fetch(PDO::FETCH_ASSOC);
-    while($row && $row2){
+function atrapperNom($nom_stock){
+    $rows = $nom_stock->fetchAll(PDO::FETCH_ASSOC, 'nom_stock', 'quantite');
+    foreach ($rows as $row){
         ?>
         <a href= "html/vendeur/produit/<?php $nom_stock ?>">
         <table>
@@ -58,7 +50,7 @@ function atrapperNom($nom_stock, $quantite){
                 <td><img src="promotion.png" alt=""> </td>
                 <td><img src="Fleche.png" alt=""> </td>
                 <td> | </td>
-                <td><?php $row2 ?></td>
+                <td><?php $row['quantite'] ?></td>
             </tr>            
         </table>
         </a>
