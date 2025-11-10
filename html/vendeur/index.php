b2950bd116cd4f8d3e679f71b1a7145723148e76
mathislqs <mathisloquais@gmail.com>
2025-11-10 17:24:25 +0100

    Create index.php

---
+++ html/vendeur/index.php
@@ -0,0 +1,90 @@
+<?php
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+    define('HOME_GIT', '../../');
+
+// Si l'utilisateur est déjà connecté
+
+if ($_POST != null){
+    require_once (HOME_GIT . 'fonction_compte.php');
+    $res = connect_compte($_POST['email'], $_POST['mdp'], 'vendeur', HOME_GIT);
+}
+
+if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true && $res['correcte']) {
+    header('location: stock');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alizon Vendeur Connexion</title>
+</head>
+<body>
+    <main>
+<?php
+    if (isset($res['fatal'])){
+?>
+        <h1 class="fatale">Désolé nous rencontrons des problèmes serveur</h1>
+<?php
+    }
+    else {
+?>
+        <form action="" method="post">
+            <legend>Connection</legend>
+<?php
+    if (isset($res['connect'])){
+?>
+        <h3 class="error"><?=$res['connect']?></h1>
+<?php
+    }
+?>
+                <!-- Adresse e-mail -->
+            <br>
+            <label for="email">Email</label>
+            <input type="email"
+                name="email"
+                id="email"
+                value="<?php if (isset($_POST['email'])) echo $_POST['email']?>"
+                required>
+<?php
+    if (isset($res['email'])){
+?>
+            <p class="error">
+                <?="Erreur : ".$res['email']?>
+            </p>
+<?php
+    }
+?>
+
+                <!-- Mot de passe -->
+            <label for="mdp">Mot de passe</label>
+            <label for="mdp">Mot de passe</label>
+            <input type="password" 
+                name="mdp"
+                id="mdp"
+                required>
+<?php
+    if (isset($res['mpd'])){
+?>
+            <p class="error">
+                <?="Erreur : ".$res['mdp']?>
+            </p>
+<?php
+    }
+?>
+            
+            <input type="submit" value="Se connecter">            
+        </form>
+        <p>Pas de compte ? <a href="../inscription/">S'inscrire</a></p>
+<?php
+    }
+?>
+    </main>
+</body>
+</html>
