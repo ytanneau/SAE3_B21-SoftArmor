8f836e2923d68662aecea4e1786f7fa03fea0865
otterspace <thomastusaccin911@gmail.com>
2025-11-10 19:28:14 +0100

    Mise en forme connexion vendeur

--- html/vendeur/index.php
+++ html/vendeur/index.php
@@ -16,6 +16,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true && $res['co
     header('location: stock');
     exit;
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -28,65 +29,54 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true && $res['co
 </head>
 <body id="connect_vendeur">
     <main>
-<?php
-    if (isset($res['fatal'])){
-?>
-        <h1 class="fatale">Désolé nous rencontrons des problèmes serveur</h1>
-<?php
-    }
-    else {
-?>
-        <form action="" method="post">
-            <legend>S'identifier</legend>
-<?php
-    if (isset($res['connect'])){
-?>
-        <h3 class="error"><?=$res['connect']?></h1>
-<?php
-    }
-?>
-                <!-- Adresse e-mail -->
-            <br>
-            <label for="email">Email</label>
-            <input type="email"
-                name="email"
-                id="email"
-                value="<?php if (isset($_POST['email'])) echo htmlentities($_POST['email'])?>"
-                required
-                class="champ">
-<?php
-    if (isset($res['email'])){
-?>
-            <p class="error">
-                <?="Erreur : ".$res['email']?>
-            </p>
-<?php
-    }
-?>
+        <?php if (isset($res['fatal'])) { ?>
+                <h1 class="fatale">Désolé, nous rencontrons des problèmes serveur</h1>
+        <?php } else { ?>
+                <form action="" method="post">
+                    <legend>S'identifier</legend>
+                    
+                    <?php if (isset($res['connect'])) { ?>
+                        <h3 class="error"><?=$res['connect']?></h1>
+                    <?php } ?>
 
-                <!-- Mot de passe -->
-            <label for="mdp">Mot de passe</label>
-            <input type="password" 
-                name="mdp"
-                id="mdp"
-                required
-                class="champ">
-<?php
-    if (isset($res['mpd'])){
-?>
-            <p class="error">
-                <?="Erreur : ".$res['mdp']?>
-            </p>
-<?php
-    }
-?>
-            
-            <input type="submit" value="Se connecter" class="boutton">            
-        </form>
-        <p>Pas de compte ? <a href="inscription/">S'inscrire</a></p>
-<?php
-    }
-?>
+                    <!-- Adresse e-mail -->
+                    <br>
+                    <label for="email">Email</label>
+                    <input type="email"
+                        name="email"
+                        id="email"
+                        value="<?php if (isset($_POST['email'])) echo htmlentities($_POST['email'])?>"
+                        required
+                        class="champ">
+
+                    <?php if (isset($res['email'])) { ?>
+                        <p class="error">
+                            <?="Erreur : ".$res['email']?>
+                        </p>
+                    <?php } ?>
+
+                    <!-- Mot de passe -->
+                    <label for="mdp">Mot de passe</label>
+                    <input type="password" 
+                        name="mdp"
+                        id="mdp"
+                        required
+                        class="champ">
+
+                    <?php if (isset($res['mdp'])) { ?>
+                        <p class="error">
+                            <?="Erreur : ".$res['mdp']?>
+                        </p>
+                    <?php } ?>
+                    
+                    <input type="submit" value="Se connecter" class="boutton">            
+                </form>
+
+                <!-- Lien vers la page d'inscription vendeur -->
+                <p>Pas de compte ? <a href="inscription/">S'inscrire</a></p>
+        <?php
+            }
+        ?>
     </main>
 </body>
 </html>
