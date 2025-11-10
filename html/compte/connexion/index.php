aff67735f66eea77c3eff63416ef0b0d96751695
otterspace <thomastusaccin911@gmail.com>
2025-11-10 13:49:55 +0100

    Test conn client

--- html/compte/connexion/index.php
+++ html/compte/connexion/index.php
@@ -3,7 +3,6 @@
 define('HOME_GIT', '../../../');
 
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-    echo "Tout marche";
     header('location: ' . HOME_GIT . "html");
     exit;
 }
