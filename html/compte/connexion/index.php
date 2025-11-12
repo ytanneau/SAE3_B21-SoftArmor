acc5c2ab9778a4e5ef0fab65fcab09cbc42e36ab
otterspace <thomastusaccin911@gmail.com>
2025-11-12 11:55:32 +0100

    Idem

--- html/compte/connexion/index.php
+++ html/compte/connexion/index.php
@@ -62,7 +62,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
                 value=""
                 class="champ">
             
-            <p class="error"><?php if (isset($erreurs['mdp'])) { $erreurs['mdp']; } ?></p>
+            <p class="error"><?php if (isset($erreurs['mdp'])) { echo $erreurs['mdp']; } ?></p>
         
             <p class="error"><?php if (isset($erreurs['connecte'])) { echo $erreurs['connecte']; } ?></p>
             
