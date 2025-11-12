38e94347b5774f97695baf10bfcf32a74ca4d44f
otterspace <thomastusaccin911@gmail.com>
2025-11-12 21:31:20 +0100

    Style ajouté à l'accueil

--- html/index.php
+++ html/index.php
@@ -37,6 +37,8 @@ unset($pdo);
 <head>
     <meta charset="UTF-8">
     <meta name="Page D'Accueil" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
     <title>Accueil</title>
 </head>
 <body>
