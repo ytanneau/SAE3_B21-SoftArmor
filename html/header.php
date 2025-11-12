d91a1f6a8d4644484bfd7c64399fc9c58d27b721
otterspace <thomastusaccin911@gmail.com>
2025-11-12 21:31:56 +0100

    Update header.php

--- html/header.php
+++ html/header.php
@@ -1,13 +1,3 @@
-<!-- Flex column -->
-<!DOCTYPE html>
-    <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
-        <link rel="stylesheet" href="<?= HOME_GIT . 'style.css' ?>">
-    </head>
-
 <header>
     <!-- Logo Alizon, barre de recherche, onglets compte/panier -->
     <!-- Flex horizontal -->
@@ -27,5 +17,3 @@
         
     </ul>
 </header>
-
-</html>
