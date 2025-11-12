6f2c59641f85a6e5554e2f1e5a598ea1b9bc9009
ytanneau <tannyann29@gmail.com>
2025-11-12 18:45:35 +0100

    Update fonction_compte.php

--- fonction_compte.php
+++ fonction_compte.php
@@ -533,13 +533,9 @@
     function sql_check_email($pdo, $email){
         try {
             //$requete = $pdo->prepare("SELECT 1 FROM compte_actif WHERE email = :email");
-            echo "test sql";
             $requete = $pdo->prepare("SELECT email_actif_existe(:email)");
-            echo "test sql2";
             $requete->bindValue(':email', $email, PDO::PARAM_STR);
-            echo "test sql3";
             $requete->execute();
-            echo "test sql4";
 
             return ($requete->fetch(PDO::FETCH_ASSOC) != null);
         } catch (PDOException $e) {
