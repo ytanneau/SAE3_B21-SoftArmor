7244e7600ac3d920025fbc7e1f65811aa7e1e314
otterspace <thomastusaccin911@gmail.com>
2025-11-13 11:34:32 +0100

    Update header client

--- html/header.php
+++ html/header.php
@@ -1,13 +1,14 @@
-<header>
+<header id="header_client">
     <!-- Logo Alizon, barre de recherche, onglets compte/panier -->
     <!-- Flex horizontal -->
     <div>
         <a href="<?= HOME_GIT ?>">
             <img src="<?= HOME_GIT . 'image/Alizon_blanc.png' ?>" alt="Logo Alizon" title="Logo Alizon">
         </a>
-
+        
         <form action="<?= HOME_GIT . 'recherche' ?>" method="get">
             <input type="search" name="recherche" id="recherche">
+            <input type="submit" value="Rechercher">
         </form>
     </div>
 
