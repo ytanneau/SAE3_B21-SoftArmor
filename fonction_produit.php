b4fbb177672687721c92a964b300e323f6414d5b
otterspace <thomastusaccin911@gmail.com>
2025-11-14 16:03:35 +0100

    Update chemins

--- fonction_produit.php
+++ fonction_produit.php
@@ -1,5 +1,5 @@
-<?php  
-    
+<?php
+
     // Fonction pour calculer et afficher les moyennes d'un produit
     function afficher_moyenne_note($moyenne){
         if($moyenne > 5 || $moyenne < 0){
@@ -7,13 +7,13 @@
         }
         // code de iwan pour calculer et afficher les moyennes d'un produit en fonction de sa moyenne
         for ($i =1; $i <= floor($moyenne); $i++){
-            ?> <img src="../../image/etoile_pleine.svg" alt="étoile pleine"><?php
+            ?> <img src="image/etoile_pleine.svg" alt="étoile pleine"><?php
         }
         if(fmod(floor($moyenne*2),2)){
-            ?> <img src="../../image/etoile_demi.svg" alt="étoile à moitié pleine"> <?php 
+            ?> <img src="image/etoile_demi.svg" alt="étoile à moitié pleine"> <?php 
         }
         for ($i =5; $i > round($moyenne); $i--){
-            ?> <img src="../../image/etoile_vide.svg" alt="étoile vide"><?php
+            ?> <img src="image/etoile_vide.svg" alt="étoile vide"><?php
         }
     }
 
