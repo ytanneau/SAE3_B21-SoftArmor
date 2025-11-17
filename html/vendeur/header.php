11074fdd004ae9bb0a49a11c3b8f57ae9fc95291
otterspace <thomastusaccin911@gmail.com>
2025-11-18 00:11:29 +0100

    Test header vendeur

--- html/vendeur/header.php
+++ html/vendeur/header.php
@@ -8,6 +8,7 @@
 
         <ul>
             <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) { ?>
+                <li> <a href="<?= HOME_SITE . 'panier/' ?>"> <img src="<?= $images . 'panier_blanc.svg' ?>" class="icon">Mon panier</a> </li>
                 <li>
                     <div class="dropdown">
                         <button onclick="ouvrirMenu()" class="dropdown-button">
