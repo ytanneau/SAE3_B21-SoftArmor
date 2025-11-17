1f3475080039d82809d168ec74550228cb37ba4d
otterspace <thomastusaccin911@gmail.com>
2025-11-17 22:23:39 +0100

    Fix vendeur

--- html/vendeur/avis/index.php
+++ html/vendeur/avis/index.php
@@ -6,11 +6,14 @@
         session_start();
     }
 
-    if (!isset($_SESSION['raison_sociale'])) {
+    // Si je suis connecté mais pas en tant que vendeur, retour à l'accueil client
+    if (!isset($_SESSION['raison_sociale']) && isset($_SESSION['logged_in']) && isset($_SESSION['raison_sociale'])) {
         header('location: ' . HOME_SITE);
         exit;
+
+    // Sinon si je ne suis pas connecté, retour à la page connexion vendeur
     } else if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] === false) {
-        header('location: ../');
+        header('location: ../../../');
         exit;
     }
 
