ba8d9255978a03fadef757dd98e427c32e955255
mathislqs <mathisloquais@gmail.com>
2025-11-18 08:50:24 +0100

    ajout de ob

--- html/vendeur/produit/index.php
+++ html/vendeur/produit/index.php
@@ -6,11 +6,13 @@
     if (!isset($_SESSION)) {
         session_start();
     }
+    ob_start();
     function renvoi(){
         if (headers_sent()) {
             die('Échec de redirection. Cliquez sur ce lien svp : <a href="../">Ici</a>');
         }
         else{
+            ob_flush();
             exit(header("Location: ../"));
         }
     }
@@ -105,5 +107,6 @@
         </main>
     </body>
 </html>
-
+<?php
+ob_end_clean();
 
