7eaa58995a8bdd002316629e7531fc3d5f7a03d2
mathislqs <mathisloquais@gmail.com>
2025-11-18 13:55:26 +0100

    normalement c bon

--- html/vendeur/produit/index.php
+++ html/vendeur/produit/index.php
@@ -65,13 +65,13 @@
             </tr>
         </table>
         <div>
-            <img src="<?= HOME_SITE . 'ressources/produit/' . $_GET['produit'] . '_' . $rows2['id_image_principale'] . '.png' ?>" > 
+            <img src="<?= HOME_SITE . 'ressources/produit/' . $_GET['produit'] . '_1.png' ?>" > 
             <?php
                 if (isset($rows2['id_image1'])) {
-                    ?><img src="<?= HOME_SITE . 'ressources/produit/' . $_GET['produit'] . '_' . $rows2['id_image1'] . '.png' ?>" > <?php
+                    ?><img src="<?= HOME_SITE . 'ressources/produit/' . $_GET['produit'] . '_2.png' ?>" > <?php
                 }
                 if (isset($rows2['id_image2'])) {
-                    ?> <img src="<?= HOME_SITE . 'ressources/produit/' . $_GET['produit'] . '_' . $rows2['id_image2'] . '.png' ?>" > <?php
+                    ?> <img src="<?= HOME_SITE . 'ressources/produit/' . $_GET['produit'] . '_3.png' ?>" > <?php
                 }
             ?>
         </div>
