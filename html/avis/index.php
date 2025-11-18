cdb39b440974b57009e5052fd4822a6a1020dbad
ytanneau <tannyann29@gmail.com>
2025-11-18 08:57:47 +0100

    Update index.php

--- html/avis/index.php
+++ html/avis/index.php
@@ -1,7 +1,7 @@
 <?php
     define('HOME_GIT', '../../');
     define('HOME_SITE', '../');
-    define ('SITE_ROOT', realpath(dirname(__FILE__)));
+    //define ('SITE_ROOT', realpath(dirname(__FILE__)));
 
     const EXISTE = "Existe déjà";
     const EXISTE_PAS = "Existe pas";
@@ -20,9 +20,9 @@
 
     if (isset($_FILES['image'])) {
         $fichier = $_SESSION['id_compte'] . '_'. time();
-        move_uploaded_file($_FILES['image']['tmp_name'], SITE_ROOT.'../ressources/avis/'. $fichier);
+        //move_uploaded_file($_FILES['image']['tmp_name'], SITE_ROOT.'../ressources/avis/'. $fichier);
 
-        //move_uploaded_file($_FILES['image']['tmp_name'], HOME_GIT . "ressources/avis/" . $fichier);
+        move_uploaded_file($_FILES['image']['tmp_name'], HOME_SITE . "ressources/avis/" . $fichier);
     }
 
     require_once HOME_GIT . 'fonction_avis.php';
