435aecc406edc746140a0738d2b06e261ec0cb9a
Iwan <iwan.villalard@etudiant.univ-rennes.fr>
2025-11-18 14:53:54 +0100

    test coordonnées

--- html/achat/index.php
+++ html/achat/index.php
@@ -290,7 +290,7 @@ else if ($numEtape == 2) {
 
         <br>
         <label for="date_exp">Date d'expiration de la carte</label>
-        <input type="text" name="date_exp" id="date_exp" size="5" maxlength="5" required>
+        <input type="text" name="date_exp" id="date_exp" required>
         <p class="contrainte">MM/YY ex: 12/25, 01/26</p>
         <?php
         if (isset($erreurs['date_exp'])){
