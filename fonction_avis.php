4ba177a5f9e63ae9e7de54f24a554d69038ac143
otterspace <thomastusaccin911@gmail.com>
2025-11-20 00:44:13 +0100

    Fix message erreur avis

--- fonction_avis.php
+++ fonction_avis.php
@@ -14,7 +14,7 @@
             $erreur['note'] = FORMAT;
         }
 
-        if (isset($_POST['description']) && !isset($_POST['titre'])){
+        if (isset($_POST['description']) && isset($_POST['titre']) && empty($_POST['titre'])){
             $erreur['titre'] = "Une description a besoin d'un titre";
         }
         else if (strlen($_POST['titre']) > TAILLE_TITRE){
