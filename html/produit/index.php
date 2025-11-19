a9e583ac723a473a8bd134c2824a397ab89dcbd4
otterspace <thomastusaccin911@gmail.com>
2025-11-19 15:08:32 +0100

    Retirer image de l'avis si inexistante

--- html/produit/index.php
+++ html/produit/index.php
@@ -129,7 +129,11 @@ if ($_POST != NULL) {
                 <p><?= htmlentities($avis['titre'] ?? '') ?></p>
                 <p><?= afficher_moyenne_note(htmlentities($avis['note'] ?? ''))?></p>
                 <p><?= htmlentities($avis['commentaire'] ?? '') ?></p>
-                <img height="200px" src="<?= HOME_SITE . $avis['url_image'] ?>" title="<?= $avis['alt_image'] ?>" alt="<?= $avis['alt_image'] ?>">
+                
+                <?php if (isset($avis['url_image'])) { ?>
+                    <img height="200px" src="<?= HOME_SITE . $avis['url_image'] ?>" title="<?= $avis['alt_image'] ?>" alt="<?= $avis['alt_image'] ?>">
+                <?php } ?>
+                
                 <p><?= 'Avis rédigé par ' . htmlentities($avis['pseudo'] ?? '') .  ' le ' . date('d/m/Y', strtotime(htmlentities($avis['date_avis'] ?? ''))) ?></p>
             </li>
         <?php } ?>
