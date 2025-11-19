21e58fe31f3e53912a54ddeaf0b34edee1a71e96
titou_qlvr <titouan.quillevere@gmail.com>
2025-11-19 16:56:04 +0100

    correction de nom de variable

--- html/vendeur/stock/modifier_produit/index.php
+++ html/vendeur/stock/modifier_produit/index.php
@@ -55,7 +55,7 @@
                         $_POST['poidColis'],
                         $_POST['volumeColis']);
         if($_POST['categorie'] != $tabCategorieDuProduit['nom_categorie']){
-            update_categorie_produit($id_produit, $tabCategorieDuProduit['nom_categorie']);
+            update_categorie_produit($idProduit, $tabCategorieDuProduit['nom_categorie']);
         }
         
         if (isset($_FILES['photoPrincipale'])){
