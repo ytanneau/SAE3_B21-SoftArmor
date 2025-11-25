7884a45ae6edeaf25cf44d8068e8da47e90a6f13
Iwan <iwan.villalard@etudiant.univ-rennes.fr>
2025-11-25 21:11:54 +0100

    include link head inscription client

--- html/compte/inscription/index.php
+++ html/compte/inscription/index.php
@@ -41,9 +41,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include HOME_SITE . 'link_head.php' ?>
     <title>Alizon - Inscription</title>
-    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
-    <link rel="stylesheet" href="<?=HOME_SITE?>style.css">
 </head>
 <body id="inscription_client">
     <main>
