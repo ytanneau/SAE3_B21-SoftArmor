06f9b1c616c4ca5b77c53dc1e502318608c12eff
Maxime <maxime.quentel@etudiant.univ-rennes1.fr>
2025-11-25 16:39:00 +0100

    deco

--- html/compte/informations/anonymisation_client/index.php
+++ html/compte/informations/anonymisation_client/index.php
@@ -75,6 +75,7 @@
 
                     $stmt = $pdo->prepare("UPDATE _compte SET email = :modifEmail, mdp = :modifMdp, supprime = :modifBoolSupprime, id_image_profil = :modifIdImageProfil, date_creation = :modifDateCreation WHERE id_compte = :id_compte");
                     $stmt->execute([':modifEmail' => $modifEmail, ':modifMdp' => $modifMdp, ':modifBoolSupprime' => $modifBoolSupprime, ':modifIdImageProfil' => $modifIdImageProfil, ':modifDateCreation' => $modifDateCreation, ':id_compte' => $id_compte]);
+                    header('location:' .HOME_SITE.'deconnexion.php');
                 }
             ?>
         </main>
