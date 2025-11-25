ae6817f33d51704e69c2f1a95d44654c338324a0
otterspace <thomastusaccin911@gmail.com>
2025-11-25 15:03:24 +0100

    Retirer taille fixe image avis

--- html/compte/informations/index.php
+++ html/compte/informations/index.php
@@ -393,10 +393,10 @@ unset($pdo);
                         <div>
                             <div>
                                 <?php if (isset($row['url_pdp'])) {?>
-                                    <img height="40px" width="40px" src="<?= HOME_SITE . $row['url_pdp'] ?>" alt="<?= htmlentities($row['alt_pdp'] ?? '')?>" title="<?= htmlentities($row['titre_pdp'] ?? '')?>">
+                                    <img src="<?= HOME_SITE . $row['url_pdp'] ?>" alt="<?= htmlentities($row['alt_pdp'] ?? '')?>" title="<?= htmlentities($row['titre_pdp'] ?? '')?>">
                                 <?php
                                     } else {?>
-                                    <img height="40px" width="40px" src="<?= HOME_SITE . 'image/compte.svg'?>">
+                                    <img src="<?= HOME_SITE . 'image/compte.svg'?>">
                                 <?php } ?>
 
                                 <div class="etoiles">
