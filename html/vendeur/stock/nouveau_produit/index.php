a83a89063c165235589346decd6665bd51c1abfc
titou_qlvr <titouan.quillevere@gmail.com>
2025-11-25 19:31:12 +0100

    Petite rectif des titles

--- html/vendeur/stock/nouveau_produit/index.php
+++ html/vendeur/stock/nouveau_produit/index.php
@@ -98,7 +98,7 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <title>Ajout produit</title>
+        <title>Alizon - Créer un produit</title>
         <?php include HOME_SITE . 'link_head.php'; ?>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="<?=HOME_SITE?>style.css">
