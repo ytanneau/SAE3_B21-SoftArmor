ed53130e75318cfeadebd60281172c7f88deea03
Iwan <iwan.villalard@etudiant.univ-rennes.fr>
2025-11-25 11:49:18 +0100

    fix test css reinitaliser mdp

--- html/compte/reinitialiser/index.php
+++ html/compte/reinitialiser/index.php
@@ -138,7 +138,7 @@ if (isset($_POST['etape']) && $_POST['etape'] === 'etape_mdp') {
     <?php include HOME_SITE . 'link_head.php'; ?>
     <title>Alizon - Réinitialiser le mot de passe</title>
 </head>
-<body class="connect_client">
+<body id="connect_client">
     <?php include HOME_SITE . 'header.php'; ?>
 
     <main>  
