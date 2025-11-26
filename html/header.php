36611080ebeb5bb1396d5fcac92b815b7fbd4c5c
otterspace <thomastusaccin911@gmail.com>
2025-11-26 13:59:44 +0100

    Header fix

--- html/header.php
+++ html/header.php
@@ -7,7 +7,11 @@
 
             
             <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) { ?>
-                <li> <a href=""> <img src="<?= $images . 'panier_blanc.svg' ?>" class="icon">Mon panier</a> </li>
+                <li> 
+                    <a href=""> 
+                        <img src="<?= $images . 'panier_blanc.svg' ?>" class="icon">Mon panier
+                    </a> 
+                </li>
 
                 <li> 
                     <a href="<?= HOME_SITE . 'compte/informations' ?>"> 
@@ -20,9 +24,11 @@
                         <img src="<?= $images . 'deconnexion_blanc.svg' ?>" class="icon">Déconnexion
                     </a> 
                 </li>
+
+
             <?php } else {
                 $param = "";
-                if (isset($_GET['id_produit'])) $param = "?id_produit=" . $_GET['id_produit']; ?>
+                if (isset($_GET['produit'])) $param = "?produit=" . $_GET['produit']; ?>
 
                 <li>
                     <a href="<?= HOME_SITE . 'compte/inscription' . $param?>"> 
@@ -45,26 +51,42 @@
                 </a> 
             </li>
             
-            <li class="hide-on-mobile">
-                <a href="<?= HOME_SITE . 'panier/' ?>">
-                    <img src="<?= $images . 'panier_blanc.svg' ?>" class="icon">Mon panier
-                </a> 
-            </li>
+            <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) { ?>
+                <li class="hide-on-mobile">
+                    <a href="<?= HOME_SITE . 'panier/' ?>">
+                        <img src="<?= $images . 'panier_blanc.svg' ?>" class="icon">Mon panier
+                    </a> 
+                </li>
 
-            <li class="hide-on-mobile">
-                <div class="dropdown">
-                    <button onclick="ouvrirMenu()" class="dropdown-button">
-                        <img src="<?= $images . 'compte_blanc.svg' ?>" class="icon">
-                        Otterspace
-                    </button>
+                <li class="hide-on-mobile">
+                    <div class="dropdown">
+                        <button onclick="ouvrirMenu()" class="dropdown-button">
+                            <img src="<?= $images . 'compte_blanc.svg' ?>" class="icon">Otterspace
+                        </button>
 
-                    <div id="dropdown-compte" class="dropdown-content">
-                        <a href="<?= HOME_SITE . 'compte/informations' ?>"> <img src="<?= $images . 'compte_blanc.svg' ?>" class="icon">Mon profil</a>
-                        <!-- <a href="#"> <img src="../image/options.svg" class="icon">Paramètres</a> -->
-                        <a href="<?= HOME_SITE . 'deconnexion' ?>"> <img src="<?= $images . 'deconnexion_blanc.svg' ?>" class="icon">Déconnexion</a>
+                        <div id="dropdown-compte" class="dropdown-content">
+                            <a href="<?= HOME_SITE . 'compte/informations' ?>"> <img src="<?= $images . 'compte_blanc.svg' ?>" class="icon">Mon profil</a>
+                            <!-- <a href="#"> <img src="../image/options.svg" class="icon">Paramètres</a> -->
+                            <a href="<?= HOME_SITE . 'deconnexion' ?>"> <img src="<?= $images . 'deconnexion_blanc.svg' ?>" class="icon">Déconnexion</a>
+                        </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            <?php } else {
+                $param = "";
+                if (isset($_GET['produit'])) $param = "?produit=" . $_GET['produit']; ?>
+
+                <li>
+                    <a href="<?= HOME_SITE . 'compte/inscription' . $param?>"> 
+                        <img src="<?= $images . 'compte_blanc.svg' ?>" class="icon">S'inscrire
+                    </a> 
+                </li>
+
+                <li>
+                    <a href="<?= HOME_SITE . 'compte/connexion' . $param?>">
+                        <img src="<?= $images . 'connexion_blanc.svg' ?>" class="icon">Se connecter
+                    </a> 
+                </li>
+            <?php } ?>
 
             <li class="menu-button" onclick=showSidebar()>
                 <img src="<?= $images . 'menu_blanc.svg' ?>">
