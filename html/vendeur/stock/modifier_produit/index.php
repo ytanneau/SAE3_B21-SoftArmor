340dc37a8b3f74c8734d285931f8d33b16017872
titou_qlvr <titouan.quillevere@gmail.com>
2025-11-26 20:03:25 +0100

    evidemment

--- html/vendeur/stock/modifier_produit/index.php
+++ html/vendeur/stock/modifier_produit/index.php
@@ -408,7 +408,7 @@
             modifierProduit.addEventListener('click' , () =>  {
                 if(nomPrv.value === ""|| nomPblc.value === "" || tva.value === "" || 
                     prix.value === "" || poidColis.value === "" || volumeColis.value === "" ||
-                    (categorie.value === "" && selectSousCategorieAlimentaire === "")||
+                    (categorie.value === "" && selectSousCategorieAlimentaire.value === "")||
                     checkCodeBarre(codeBarre.value)){
                     alert("Les champs obligatoires ne sont pas tous remplis");
                     event.preventDefault();
