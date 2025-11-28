97ba829570c65c6833ac4dfe08c3331e239b241b
Iwan <iwan.villalard@etudiant.univ-rennes.fr>
2025-11-28 09:09:13 +0100

    style bouton annulation

--- html/compte/informations/anonymisation_client/index.php
+++ html/compte/informations/anonymisation_client/index.php
@@ -37,7 +37,7 @@
             <form action="" name="formulaireModif" method="post" enctype="multipart/form-data">
                 <input type="submit" value="Confirmer la désactivation du compte">
             </form>
-            <button><a href="..">Annuler la désactivation</a></button>
+            <a href=".."><button class="bouton">Annuler la désactivation</button></a>
             <?php
                 if($_SERVER["REQUEST_METHOD"] == "POST"){
                     // récupération des données du formulaire de saisie
