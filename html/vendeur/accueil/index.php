2ed75daf63a778d1ecef6de1f577e7c4919bc718
titou_qlvr <titouan.quillevere@gmail.com>
2026-01-05 14:30:41 +0100

    ajout d'une page d'acceuil

--- html/vendeur/accueil/index.php
+++ html/vendeur/accueil/index.php
@@ -30,7 +30,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <?php include HOME_SITE . 'link_head.php'; ?>
-        <title>Alizon - Inscription</title>
+        <title>Alizon - Accueil vendeur</title>
     </head>
     <body>
         
