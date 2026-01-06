94105237a7aba07dd21ce318cd3f77d02ae88eeb
otterspace <thomastusaccin911@gmail.com>
2026-01-06 17:21:20 +0100

    Fix form recherche

--- html/recherche/index.php
+++ html/recherche/index.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION)) {
     if (isset($_SESSION['raison_sociale'])){
         header('location: /vendeur/stock/');
         die();
-    } else if (!isset($produit)) {
+    } else if (!isset($recherche)) {
         header('location: ' . HOME_SITE);
         die();
     }
