68e3af3486af2cd77f666cef83a41ee17cd01907
otterspace <thomastusaccin911@gmail.com>
2026-01-07 14:57:48 +0100

    Test recherche

--- fonction_recherche.php
+++ fonction_recherche.php
@@ -3,8 +3,8 @@
 function get_produits_recherche($recherche) {
     global $pdo;
         
-    $requete = $pdo->prepare("SELECT * FROM produit_visible WHERE nom_public LIKE '%:recherche%'");
-    $requete->bindValue(':recherche', $recherche, PDO::PARAM_STR);
+    $requete = $pdo->prepare("SELECT * FROM produit_visible WHERE nom_public LIKE '%Kouign%'");
+    //$requete->bindValue(':recherche', $recherche, PDO::PARAM_STR);
     $requete->execute();
     return $requete->fetch(PDO::FETCH_ASSOC);
 }
