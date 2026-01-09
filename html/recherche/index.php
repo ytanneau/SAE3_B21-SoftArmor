e1d4805bffc3e47d7a66cb65b9a7f65f9fdb73fc
otterspace <thomastusaccin911@gmail.com>
2026-01-09 11:26:57 +0100

    Fix filtre

--- html/recherche/index.php
+++ html/recherche/index.php
@@ -84,7 +84,7 @@ require_once (HOME_GIT . 'fonction_recherche.php');
 
     // Callable permettant de cumuler tous les filtres actifs
     const combineFilters = (...filters) => (item) => {
-        return filters.map((filter) => filter(item)).every((x) => x === true)
+        return filters.map((filter) => filter(item)).every((x) => x === true);
     };
 
     // On met un Listener sur le div contenant toutes les checkbox pour détecter les changements
