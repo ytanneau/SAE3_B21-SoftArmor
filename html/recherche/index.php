138363848fc2408a06bf07cb8ca7aa8b967724ab
mathislqs <mathisloquais@gmail.com>
2026-01-12 09:04:10 +0100

    radio

--- html/recherche/index.php
+++ html/recherche/index.php
@@ -45,19 +45,19 @@ require_once (HOME_GIT . 'fonction_recherche.php');
             <fieldset>
                 <legend>Filtrer par prix</legend>
 
-                <input type="checkbox" name="prix" id="zeroTo20" value="zeroTo20">
+                <input type="radio" name="prix" id="zeroTo20" value="zeroTo20">
                 <label for="zeroTo20">0 € à 20 €</label>
 
-                <input type="checkbox" name="prix" id="twentyTo50" value="twentyTo50">
+                <input type="radio" name="prix" id="twentyTo50" value="twentyTo50">
                 <label for="twentyTo50">20 € à 50 €</label>
 
-                <input type="checkbox" name="prix" id="fiftyTo100" value="fiftyTo100">
+                <input type="radio" name="prix" id="fiftyTo100" value="fiftyTo100">
                 <label for="fiftyTo100">50 € à 100 €</label>
 
-                <input type="checkbox" name="prix" id="hundredTo300" value="hundredTo300">
+                <input type="radio" name="prix" id="hundredTo300" value="hundredTo300">
                 <label for="hundredTo300">100 € à 300 €</label>
 
-                <input type="checkbox" name="prix" id="over300" value="over300">
+                <input type="radio" name="prix" id="over300" value="over300">
                 <label for="over300">Plus de 300 €</label>
             </fieldset>
         </form>
@@ -69,87 +69,87 @@ require_once (HOME_GIT . 'fonction_recherche.php');
         <!-- Grille des résultats -->
         <div id="results"></div>
     </section>
-</body>
 
-<script type="text/javascript">
-    const searchState = {
-        search: "<?=$recherche?>",
-        filters: {
-            category: [],
-            price: {min: null, max: null},
-            sales: false
-        },
-        sort: {
-            field: "nom_public",
-            order: "asc"
-        }
-    };
 
-    // Est-on déjà sur la page de recherche ?
-    const isSearchPage = document.body.dataset.page === "search";
-    const form = document.querySelector("#form_recherche");
-    const input = document.querySelector("#recherche");
-    const resultsFor = document.querySelector("#results_for");
-
-    // Une fois la page chargée, récupérer une première fois les produits avec la recherche initiale
-    document.addEventListener("DOMContentLoaded", () => {
-        fetchProduitsJSON();
-    });
-
-    if (form) {
-        form.addEventListener("submit", (e) => {
-            if (isSearchPage) {
-                e.preventDefault();
-
-                searchState.search = input.value;
-                // searchState.page = 1;
-
-                fetchProduitsJSON();
-                resultsFor.textContent = `Résultats pour "${input.value}"`;
+    <script type="text/javascript">
+        const searchState = {
+            search: "<?=$recherche?>",
+            filters: {
+                category: [],
+                price: {min: null, max: null},
+                sales: false
+            },
+            sort: {
+                field: "nom_public",
+                order: "asc"
             }
+        };
+
+        // Est-on déjà sur la page de recherche ?
+        const isSearchPage = document.body.dataset.page === "search";
+        const form = document.querySelector("#form_recherche");
+        const input = document.querySelector("#recherche");
+        const resultsFor = document.querySelector("#results_for");
+
+        // Une fois la page chargée, récupérer une première fois les produits avec la recherche initiale
+        document.addEventListener("DOMContentLoaded", () => {
+            fetchProduitsJSON();
         });
-    }
 
-    // Récupérer tous les produits dans un objet JSON
-    async function fetchProduitsJSON() {
-        fetch('/recherche/produits.php', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(searchState)
-        })
-        .then(res => res.json())
-        .then(data => {
-            afficherProduits(data);
-        });
-    }
+        if (form) {
+            form.addEventListener("submit", (e) => {
+                if (isSearchPage) {
+                    e.preventDefault();
 
-    function afficherProduits(data) {
-        const resultGrid = document.querySelector("#results");
-        // console.log(data.produits);
+                    searchState.search = input.value;
+                    // searchState.page = 1;
 
-        // Exemple pour créer une balise
-
-        // let monAdr = document.createElement("a");
-        // let attribut = document.createAttribute("href");
-        // attribut.value = "mailto:jbond@scot-yard.uk";
-        // monAdr.setAttributeNode(attribut);
-        // monContact.appendChild(monAdr);
-
-        // Vider les produits déjà présents dans la grille
-        while (resultGrid.firstChild) {
-            resultGrid.removeChild(resultGrid.firstChild);
+                    fetchProduitsJSON();
+                    resultsFor.textContent = `Résultats pour "${input.value}"`;
+                }
+            });
         }
 
-        data.produits.forEach(produit => {
-            let paragraphe = document.createElement("p");
-            let texteNom = document.createTextNode(produit.nom_public);
-            paragraphe.appendChild(texteNom);
-    
-            resultGrid.appendChild(paragraphe);
-        });
-    }
-</script>
+        // Récupérer tous les produits dans un objet JSON
+        async function fetchProduitsJSON() {
+            fetch('/recherche/produits.php', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(searchState)
+            })
+            .then(res => res.json())
+            .then(data => {
+                afficherProduits(data);
+            });
+        }
 
+        function afficherProduits(data) {
+            const resultGrid = document.querySelector("#results");
+            // console.log(data.produits);
+
+            // Exemple pour créer une balise
+
+            // let monAdr = document.createElement("a");
+            // let attribut = document.createAttribute("href");
+            // attribut.value = "mailto:jbond@scot-yard.uk";
+            // monAdr.setAttributeNode(attribut);
+            // monContact.appendChild(monAdr);
+
+            // Vider les produits déjà présents dans la grille
+            while (resultGrid.firstChild) {
+                resultGrid.removeChild(resultGrid.firstChild);
+            }
+
+            data.produits.forEach(produit => {
+                let paragraphe = document.createElement("p");
+                let texteNom = document.createTextNode(produit.nom_public);
+                paragraphe.appendChild(texteNom);
+        
+                resultGrid.appendChild(paragraphe);
+            });
+        }
+    </script>
+</body>
 <!--
 <a href="">
     <img src="" title="" alt="">
