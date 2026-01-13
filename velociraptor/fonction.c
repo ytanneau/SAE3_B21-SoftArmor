671955ecbdcab7dea070fc7cba9c63eca018e25c
ytanneau <tannyann29@gmail.com>
2026-01-13 14:35:18 +0100

    test info colis

--- velociraptor/fonction.c
+++ velociraptor/fonction.c
@@ -356,7 +356,7 @@ bool colis_existe(MYSQL *conn, int cnx, char *code)
 
     char sql[200];
     code[BORDEREAU_SIZE-1] = ' ';
-    sprintf(sql, "SELECT * FROM _colis WHERE bordereau = '%s'", code);
+    sprintf(sql, "SELECT * FROM _colis WHERE bordereau = '%s';", code);
     printf("sql : %s\n", sql);
     if (mysql_query(conn, sql)) 
     { 
