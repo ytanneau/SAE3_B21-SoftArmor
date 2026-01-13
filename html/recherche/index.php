cec0e3aba0af78cf1da31a7c8f9ef63137a69d92
otterspace <thomastusaccin911@gmail.com>
2026-01-13 14:39:54 +0100

    Rajout main recherche

--- html/recherche/index.php
+++ html/recherche/index.php
@@ -40,28 +40,30 @@ require_once (HOME_GIT . 'fonction_recherche.php');
 <body data-page="search">
     <?php include HOME_SITE . "header.php"; ?>
 
-    <section class="filters">
-        <form>
-            <fieldset>
-                <legend>Filtrer par prix</legend>
+    <main>
+        <section class="filters">
+            <form>
+                <fieldset>
+                    <legend>Filtrer par prix</legend>
 
-                <input type="radio" name="prix" id="zeroTo20" value="zeroTo20">
-                <label for="zeroTo20">0 € à 20 €</label>
+                    <input type="radio" name="prix" id="zeroTo20" value="zeroTo20">
+                    <label for="zeroTo20">0 € à 20 €</label>
 
-                <input type="radio" name="prix" id="twentyTo50" value="twentyTo50">
-                <label for="twentyTo50">20 € à 50 €</label>
+                    <input type="radio" name="prix" id="twentyTo50" value="twentyTo50">
+                    <label for="twentyTo50">20 € à 50 €</label>
 
-                <input type="radio" name="prix" id="fiftyTo100" value="fiftyTo100">
-                <label for="fiftyTo100">50 € à 100 €</label>
+                    <input type="radio" name="prix" id="fiftyTo100" value="fiftyTo100">
+                    <label for="fiftyTo100">50 € à 100 €</label>
 
-                <input type="radio" name="prix" id="hundredTo300" value="hundredTo300">
-                <label for="hundredTo300">100 € à 300 €</label>
+                    <input type="radio" name="prix" id="hundredTo300" value="hundredTo300">
+                    <label for="hundredTo300">100 € à 300 €</label>
 
-                <input type="radio" name="prix" id="over300" value="over300">
-                <label for="over300">Plus de 300 €</label>
-            </fieldset>
-        </form>
-</section>
+                    <input type="radio" name="prix" id="over300" value="over300">
+                    <label for="over300">Plus de 300 €</label>
+                </fieldset>
+            </form>
+        </section>
+
         <select id="tri" value ="triOption">
             <option></option>
             <option value="triAvis">Par notes</option>
@@ -70,208 +72,208 @@ require_once (HOME_GIT . 'fonction_recherche.php');
             <option value="triPrixCroi">Par prix réduction</option>
         </select>
 
-<h1 id="results_for">Résultats pour "<?= $recherche ?>"</h1>
+        <h1 id="results_for">Résultats pour "<?= $recherche ?>"</h1>
 
-<section class="results">
-    <!-- Grille des résultats -->
-    <div id="results"></div>
-</section>
+        <section class="results">
+            <!-- Grille des résultats -->
+            <div id="results"></div>
+        </section>
+    </main>
 
+    <script type="text/javascript">
+        const searchState = {
+            search: "<?=$recherche?>",
+            filters: {
+                category: [],
+                price: {min: null, max: null},
+                sales: false
+            },
+            sort: {
+                field: "nom_public",
+                order: "asc"
+            }
+        };
 
-<script type="text/javascript">
-    const searchState = {
-        search: "<?=$recherche?>",
-        filters: {
-            category: [],
-            price: {min: null, max: null},
-            sales: false
-        },
-        sort: {
-            field: "nom_public",
-            order: "asc"
-        }
-    };
+        // Est-on déjà sur la page de recherche ?
+        const isSearchPage = document.body.dataset.page === "search";
+        const form = document.querySelector("#form_recherche");
+        const input = document.querySelector("#recherche");
+        const resultsFor = document.querySelector("#results_for");
 
-    // Est-on déjà sur la page de recherche ?
-    const isSearchPage = document.body.dataset.page === "search";
-    const form = document.querySelector("#form_recherche");
-    const input = document.querySelector("#recherche");
-    const resultsFor = document.querySelector("#results_for");
+        //listener pour les tris et renvoi 
+        var s = document.getElementById("tri");
+        var selNum = s.options[s.selectedIndex].value;
+        var selName = s.options[s.selectedIndex].dataset.name;
 
-    //listener pour les tris et renvoi 
-    var s = document.getElementById("tri");
-    var selNum = s.options[s.selectedIndex].value;
-    var selName = s.options[s.selectedIndex].dataset.name;
-
-    s.addEventListener("change", (e) => {
-        if (isSearchPage) {
-            e.preventDefault();
-
-            searchState.sort.field = selNum;
-            searchState.sort.order = selName;
-            console.log(searchState.sort.order);
-            console.log(searchState.sort.field);
-        }
-    });
-
-    // Une fois la page chargée, récupérer une première fois les produits avec la recherche initiale
-    document.addEventListener("DOMContentLoaded", () => {
-        // Rediriger si la recherche est vide
-        if (searchState.search === "") {
-            window.location.replace("..");
-        }
-
-        fetchProduitsJSON();
-    });
-
-    if (form) {
-        form.addEventListener("submit", (e) => {
+        s.addEventListener("change", (e) => {
             if (isSearchPage) {
                 e.preventDefault();
-                
-                // Rediriger si la recherche est vide
-                if (input.value.trim() === "") {
-                    window.location.replace("..");
+
+                searchState.sort.field = selNum;
+                searchState.sort.order = selName;
+                console.log(searchState.sort.order);
+                console.log(searchState.sort.field);
+            }
+        });
+
+        // Une fois la page chargée, récupérer une première fois les produits avec la recherche initiale
+        document.addEventListener("DOMContentLoaded", () => {
+            // Rediriger si la recherche est vide
+            if (searchState.search === "") {
+                window.location.replace("..");
+            }
+
+            fetchProduitsJSON();
+        });
+
+        if (form) {
+            form.addEventListener("submit", (e) => {
+                if (isSearchPage) {
+                    e.preventDefault();
+                    
+                    // Rediriger si la recherche est vide
+                    if (input.value.trim() === "") {
+                        window.location.replace("..");
+                    }
+                    
+                    searchState.search = input.value.trim();
+                    // searchState.page = 1;
+                    fetchProduitsJSON();
+                }
+            });
+        }
+
+        function afficherProduits(data) {
+            const resultGrid = document.querySelector("#results");
+
+            // Vider les produits déjà présents dans la grille
+            while (resultGrid.firstChild) {
+                resultGrid.removeChild(resultGrid.firstChild);
+            }
+
+            resultsFor.textContent = `${data.total} résultat${data.total > 1 ? 's' : ''} pour "${searchState.search}"`;
+
+            console.log(data.produits);
+
+            data.produits.forEach(produit => {
+                // Lien vers la page produit
+                let lien = document.createElement("a");
+                let ref = document.createAttribute("href");
+                ref.value = `/produit/?produit=${produit.id_produit}`;
+                lien.setAttributeNode(ref);
+
+                // Image du produit
+
+                let photo = image(`../${produit.url_image}`, produit.alt, produit.titre);
+
+                // Titre du produit
+                let titreNomPublic = document.createElement("h3");
+                let nomPublic = document.createTextNode(produit.nom_public);
+                titreNomPublic.appendChild(nomPublic);
+
+                // Étoiles
+                let divEtoiles = listeEtoiles(produit.note_moy);
+
+                // Si le produit n'est pas noté, l'indiquer
+                if (divEtoiles == null) {
+                    divEtoiles = document.createElement("div");
+
+                    let pNonNote = document.createElement("p");
+                    let texteNonNote = document.createTextNode("Produit Non Noté");
+
+                    pNonNote.appendChild(texteNonNote);
+                    divEtoiles.appendChild(pNonNote);
                 }
                 
-                searchState.search = input.value.trim();
-                // searchState.page = 1;
-                fetchProduitsJSON();
-            }
-        });
-    }
+                // Prix du produit (ancien prix si réduction)
+                let pPrix = document.createElement("p");
+                let prix = document.createTextNode(`${Number.parseFloat(produit.prix).toFixed(2)} €`);
+                pPrix.appendChild(prix);
 
-    function afficherProduits(data) {
-        const resultGrid = document.querySelector("#results");
+                let pPrixReduit = null;
 
-        // Vider les produits déjà présents dans la grille
-        while (resultGrid.firstChild) {
-            resultGrid.removeChild(resultGrid.firstChild);
-        }
-
-        resultsFor.textContent = `${data.total} résultat${data.total > 1 ? 's' : ''} pour "${searchState.search}"`;
-
-        console.log(data.produits);
-
-        data.produits.forEach(produit => {
-            // Lien vers la page produit
-            let lien = document.createElement("a");
-            let ref = document.createAttribute("href");
-            ref.value = `/produit/?produit=${produit.id_produit}`;
-            lien.setAttributeNode(ref);
-
-            // Image du produit
-
-            let photo = image(`../${produit.url_image}`, produit.alt, produit.titre);
-
-            // Titre du produit
-            let titreNomPublic = document.createElement("h3");
-            let nomPublic = document.createTextNode(produit.nom_public);
-            titreNomPublic.appendChild(nomPublic);
-
-            // Étoiles
-            let divEtoiles = listeEtoiles(produit.note_moy);
-
-            // Si le produit n'est pas noté, l'indiquer
-            if (divEtoiles == null) {
-                divEtoiles = document.createElement("div");
-
-                let pNonNote = document.createElement("p");
-                let texteNonNote = document.createTextNode("Produit Non Noté");
-
-                pNonNote.appendChild(texteNonNote);
-                divEtoiles.appendChild(pNonNote);
-            }
-            
-            // Prix du produit (ancien prix si réduction)
-            let pPrix = document.createElement("p");
-            let prix = document.createTextNode(`${Number.parseFloat(produit.prix).toFixed(2)} €`);
-            pPrix.appendChild(prix);
-
-            let pPrixReduit = null;
-
-            // Si le produit est en réduction, l'indiquer
-            if (produit.prix_actuel != null) {
-                pPrixReduit = document.createElement("p");
-                let prixReduit = document.createTextNode(`${Number.parseFloat(produit.prix_actuel).toFixed(2)} €`);
-                pPrixReduit.appendChild(prixReduit);
-                
-                pPrix.classList.add("ancien_prix");
-                pPrixReduit.classList.add("prix");
-            } else {
-                pPrix.classList.add("prix");
-            }
-    
-            lien.appendChild(photo);
-            lien.appendChild(titreNomPublic);
-            lien.appendChild(pPrix);
-
-            if (produit.prix_actuel != null) {
-                lien.appendChild(pPrixReduit);
-            }
-
-            lien.appendChild(divEtoiles);
-
-            resultGrid.appendChild(lien);
-        });
-    }
-
-    function listeEtoiles(moyenne) {
-        let divEtoiles = document.createElement("div");
-
-        if (moyenne == null || moyenne < 0 || moyenne > 5) {
-            return null;
-        }
-
-        for (let i = 0; i < Math.floor(moyenne); i++) {
-            divEtoiles.appendChild(image('/image/etoile_pleine.svg', 'étoile pleine', 'étoile pleine'));
-        }
-
-        if (Math.floor(moyenne * 2) % 2 != 0) {
-            divEtoiles.appendChild(image('/image/etoile_demi.svg', 'étoile à moitié pleine', 'étoile à moitié pleine'));
-        }
-
-        for (let i = 5; i > Math.round(moyenne); i--) {
-            divEtoiles.appendChild(image('/image/etoile_vide.svg', 'étoile vide', 'étoile vide'));
-        }
-
-        return divEtoiles;
-    }
-
-    function image(src, alt, title) {
-        let img = document.createElement("img");
-
-        let srcAtt = document.createAttribute("src");
-        let altAtt = document.createAttribute("alt");
-        let titleAtt = document.createAttribute("title");
-
-        srcAtt.value = src;
-        altAtt.value = alt;
-        titleAtt.value = title;
-
-        img.setAttributeNode(srcAtt);
-        img.setAttributeNode(altAtt);
-        img.setAttributeNode(titleAtt);
-
-        return img;
-    }
+                // Si le produit est en réduction, l'indiquer
+                if (produit.prix_actuel != null) {
+                    pPrixReduit = document.createElement("p");
+                    let prixReduit = document.createTextNode(`${Number.parseFloat(produit.prix_actuel).toFixed(2)} €`);
+                    pPrixReduit.appendChild(prixReduit);
                     
+                    pPrix.classList.add("ancien_prix");
+                    pPrixReduit.classList.add("prix");
+                } else {
+                    pPrix.classList.add("prix");
+                }
+        
+                lien.appendChild(photo);
+                lien.appendChild(titreNomPublic);
+                lien.appendChild(pPrix);
 
-    // Récupérer tous les produits dans un objet JSON
-    async function fetchProduitsJSON() {
-        fetch('/recherche/produits.php', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(searchState)
-        })
-        .then(res => res.json())
-        .then(data => {
-            afficherProduits(data);
-        });
-    }
-</script>
+                if (produit.prix_actuel != null) {
+                    lien.appendChild(pPrixReduit);
+                }
+
+                lien.appendChild(divEtoiles);
+
+                resultGrid.appendChild(lien);
+            });
+        }
+
+        function listeEtoiles(moyenne) {
+            let divEtoiles = document.createElement("div");
+
+            if (moyenne == null || moyenne < 0 || moyenne > 5) {
+                return null;
+            }
+
+            for (let i = 0; i < Math.floor(moyenne); i++) {
+                divEtoiles.appendChild(image('/image/etoile_pleine.svg', 'étoile pleine', 'étoile pleine'));
+            }
+
+            if (Math.floor(moyenne * 2) % 2 != 0) {
+                divEtoiles.appendChild(image('/image/etoile_demi.svg', 'étoile à moitié pleine', 'étoile à moitié pleine'));
+            }
+
+            for (let i = 5; i > Math.round(moyenne); i--) {
+                divEtoiles.appendChild(image('/image/etoile_vide.svg', 'étoile vide', 'étoile vide'));
+            }
+
+            return divEtoiles;
+        }
+
+        function image(src, alt, title) {
+            let img = document.createElement("img");
+
+            let srcAtt = document.createAttribute("src");
+            let altAtt = document.createAttribute("alt");
+            let titleAtt = document.createAttribute("title");
+
+            srcAtt.value = src;
+            altAtt.value = alt;
+            titleAtt.value = title;
+
+            img.setAttributeNode(srcAtt);
+            img.setAttributeNode(altAtt);
+            img.setAttributeNode(titleAtt);
+
+            return img;
+        }
+                        
+
+        // Récupérer tous les produits dans un objet JSON
+        async function fetchProduitsJSON() {
+            fetch('/recherche/produits.php', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(searchState)
+            })
+            .then(res => res.json())
+            .then(data => {
+                afficherProduits(data);
+            });
+        }
+    </script>
+
+    <?php include HOME_SITE . "footer.php" ?>
 </body>
-
-
 
 </html>
