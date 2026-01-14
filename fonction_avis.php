50ada497ce068a25983067864a0048cc3cf4a86e
Maxime <maxime.quentel@etudiant.univ-rennes1.fr>
2026-01-14 17:00:48 +0100

    ajout fonction car ca rendait fou

--- fonction_avis.php
+++ fonction_avis.php
@@ -120,8 +120,8 @@
         return $requete->fetch(PDO::FETCH_ASSOC);
     }
 
-    // fonction pour supprimer un avis d'un client sur un produit
-    function supprimer_avis($id_produit, $url_img_avis, $id_client){
+    // fonction pour supprimer un avis avec image d'un client sur un produit
+    function supprimer_avis_image($id_produit, $url_img_avis, $id_client){
         global $pdo;
         try {
             $requete = $pdo->prepare("DELETE FROM _avis WHERE id_produit = :id_produit AND id_client = :id_client;");
@@ -141,4 +141,22 @@
             throw $e;
         }
     }
+
+    // fonction pour supprimer un avis sans image d'un client sur un produit
+    function supprimer_avis($id_produit, $id_client){
+        global $pdo;
+        try {
+            $requete = $pdo->prepare("DELETE FROM _avis WHERE id_produit = :id_produit AND id_client = :id_client;");
+            $requete->bindValue(':id_produit', $id_produit, PDO::PARAM_INT);
+            $requete->bindValue(':id_client', $id_client, PDO::PARAM_INT);
+            $requete->execute();
+            
+            
+            return 0;
+        } catch (PDOException $e) {
+            throw $e;
+        }
+    }
+    
+?>
 
