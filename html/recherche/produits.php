6d40342d2402cf3dc75cf8b82ba8bb529f1850b6
mathislqs <mathisloquais@gmail.com>
2026-01-14 10:15:35 +0100

    Update produits.php

--- html/recherche/produits.php
+++ html/recherche/produits.php
@@ -35,15 +35,10 @@
     }
     else {
         $requete .= " ORDER BY :field :order nom_public ASC";
-        $params['field'] = $sort['field'];
-        $params['order'] = $sort['order'];
+        $params[':field'] = $sort['field'];
+        $params[':order'] = $sort['order'];
     }
     
-    // <option value="aToZ" data-name ="asc">choisissez un tri :</option>
-    //         <option value="triAvis" data-name ="asc">Par notes</option>
-    //         <option value="triPrix" data-name ="asc">Par prix decroissant</option>
-    //         <option value="triPrixCroi" data-name ="desc">Par prix croissant</option>
-    //         <option value="triPrixCroi" data-name ="asc">Par prix réduction</option>
 
     $requete = $pdo->prepare($requete);
     $requete->execute($params);
