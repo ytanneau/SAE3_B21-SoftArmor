2f8f825cb78a099cf0bd2479c8efa56f18189e2d
mathislqs <mathisloquais@gmail.com>
2026-01-14 16:32:45 +0100

    Update index.php

--- html/recherche/index.php
+++ html/recherche/index.php
@@ -144,7 +144,7 @@ require_once (HOME_GIT . 'fonction_recherche.php');
 
         function afficherProduits(data) {
             const resultGrid = document.querySelector("#results");
-
+            console.log(resultGrid);
             // Vider les produits déjà présents dans la grille
             while (resultGrid.firstChild) {
                 resultGrid.removeChild(resultGrid.firstChild);
