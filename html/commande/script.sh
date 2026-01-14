a5c136a5fdf90ed6fea06513a8dbf1afb534500f
Iwan <villalard.iwan@outlook.com>
2026-01-14 17:43:09 +0100

    impression de commande

--- html/commande/script.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/sh
-
-docker container run -v ~/Documents/Alizon/SAE/html/commande/work:/work bigpapoo/html2pdf
-mv test.html work/
-cp script_conversion.sh work/
-# sh work/script_conversion.sh FAUT FAIRE PAR LE CONTAINEUR
-cp work/result.pdf ./
