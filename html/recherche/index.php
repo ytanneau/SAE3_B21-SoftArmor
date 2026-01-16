b982dc425d99164dd05573903e7ea96cf4c791db
mathislqs <mathisloquais@gmail.com>
2026-01-16 09:40:07 +0100

    Update index.php

--- html/recherche/index.php
+++ html/recherche/index.php
@@ -60,12 +60,15 @@ require_once (HOME_GIT . 'fonction_recherche.php');
 
                     <input type="radio" name="prix" id="over300" value="over300">
                     <label for="over300">Plus de 300 €</label>
+
+                    <input type="checkbox" name="prom" id="prom" value="prom">
+                    <label for="over300">Promotion</label>
                 </fieldset>
             </form>
         </section>
         <label for="tri">Trier par </label>
         <select id="tri" value ="triOption">
-            <option value="nom_public" data-name ="ASC">ordre alphabtique</option>
+            <option value="nom_public" data-name ="ASC">Ordre alphabétique</option>
             <option value="note_moy" data-name ="DESC">Meilleurs avis</option>
             <option value="triPrix" data-name ="ASC">Prix croissants</option>
             <option value="triPrixCroi" data-name ="DESC">Prix décroissants</option>
@@ -99,7 +102,14 @@ require_once (HOME_GIT . 'fonction_recherche.php');
         const form = document.querySelector("#form_recherche");
         const input = document.querySelector("#recherche");
         const resultsFor = document.querySelector("#results_for");
+        
+        let p = document.getElementById("prom");
 
+        p.addEventListener("checked", (e) => {
+            e.preventDefault();
+            let selProm = p.querySelector('messageCheckbox:checked').value;
+            console.log(selProm);
+        });
         //listener pour les tris et renvoi 
         let s = document.getElementById("tri");
 
