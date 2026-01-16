5205a82d94a161d45b273fa788022b9bb33d3c9f
Iwan <villalard.iwan@outlook.com>
2026-01-16 11:07:56 +0100

    suppression produit du panier (pour visiteur)

--- html/panier/index.php
+++ html/panier/index.php
@@ -23,7 +23,25 @@ require_once (HOME_GIT . 'fonction_produit.php');
 //supprime le produit selectionné
 if ($_POST != NULL) {
     $id_prod = $_POST['id_produit'];
-    supprimer_produit_panier($id_prod,$id_client);
+
+    if (isset($_SESSION['id_compte'])) {
+        supprimer_produit_panier($id_prod,$id_client);
+    } else {
+        $panier = unserialize($_COOKIE['panier']);
+
+        for ($i = 0; $i < count($panier); $i++) {
+            if ($panier[$i]['id_produit'] == $id_prod) {
+                unset($panier[$i]);
+                $panier = array_values($panier);
+                break;
+            }
+        }
+
+        $panier_seria = serialize($panier);
+
+        $_COOKIE['panier'] = $panier_seria;
+        setcookie('panier', $panier_seria, path:'/');
+    }
 }
 
 if (isset($_SESSION['logged_in'])) {
