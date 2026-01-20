bcfd66e21b26c6f6c8a4fee9fae13f8242b874f7
otterspace <thomastusaccin911@gmail.com>
2026-01-20 18:08:31 +0100

    Test CSS dropdown

--- html/toolbar_categories.php
+++ html/toolbar_categories.php
@@ -3,27 +3,32 @@
 ?>
 
 <div class="toolbar" id="categories">
-    <?php foreach ($categories as $cat) { 
-        $sous_cats = get_sous_categorie($cat['nom_categorie']);
-        $chemin = HOME_SITE . 'recherche/?categorie=' . urlencode($cat['nom_categorie']);
-
-        // Si sous-catégories, menu déroulant
-        if (!empty($sous_cats)) { ?>
-            <div class="dropdown">
-                <a class="dropbown-button" href="<?= $chemin ?>"><?= $cat['nom_categorie'] ?></a>
-
-                <div class="dropdown-content">
-                    <?php foreach ($sous_cats as $sous_cat) { 
-                        $chemin_sous_cat = 'recherche/?categorie=' . urlencode($sous_cat['nom_categorie']); ?>
-                    
-                        <a href="<?= $chemin_sous_cat ?>"><?= $sous_cat['nom_categorie'] ?></a>
-                    <?php } ?>
-                </div>
-            </div>
+    <ul>
+        <?php foreach ($categories as $cat) { ?>
+            <li>
+                <?php
+                $sous_cats = get_sous_categorie($cat['nom_categorie']);
+                $chemin = HOME_SITE . 'recherche/?categorie=' . urlencode($cat['nom_categorie']);
         
-        <!-- Si pas de sous-catégorie, simple lien -->
-        <?php } else { ?>
-            <a href="#" href="<?= $chemin ?>"><?= $cat['nom_categorie'] ?></a>
+                // Si sous-catégories, menu déroulant
+                if (!empty($sous_cats)) { ?>
+                    <div class="dropdown">
+                        <a class="dropbown-button" href="<?= $chemin ?>"><?= $cat['nom_categorie'] ?></a>
+        
+                        <div class="dropdown-content">
+                            <?php foreach ($sous_cats as $sous_cat) { 
+                                $chemin_sous_cat = 'recherche/?categorie=' . urlencode($sous_cat['nom_categorie']); ?>
+                            
+                                <a href="<?= $chemin_sous_cat ?>"><?= $sous_cat['nom_categorie'] ?></a>
+                            <?php } ?>
+                        </div>
+                    </div>
+                
+                <!-- Si pas de sous-catégorie, simple lien -->
+                <?php } else { ?>
+                    <a href="#" href="<?= $chemin ?>"><?= $cat['nom_categorie'] ?></a>
+                <?php } ?>
+            </li>
         <?php } ?>
-    <?php } ?>
+    </ul>
 </div>
