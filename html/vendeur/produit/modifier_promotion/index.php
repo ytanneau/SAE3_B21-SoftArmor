2cd25aa7b914e0c88e527892750a29c639749f57
titou_qlvr <titouan.quillevere@gmail.com>
2026-01-20 14:35:54 +0100

    azdezds

--- html/vendeur/produit/modifier_promotion/index.php
+++ html/vendeur/produit/modifier_promotion/index.php
@@ -152,11 +152,11 @@
                 <p style="display:none; color:red;" id="warning2">Date(s) non selectionné(s)</p>
                 
                 <?php if($tab_image_promotion != null){ ?>
-                    <div class="en_colonne" style="width:fit-content;">
+                    <div class="en_ligne" style="width:fit-content;">
                         <img src=<?= HOME_SITE . $tab_image_promotion['url_image']?> alt="Banniere de promotion">
-                        <label class="hide_input" for="photoPromotion">Changer la banniere</label>
-                        <input style="display:none;" type="file" id="photoPromotion" name="photoPromotion" accept=".png">
                         <div class="zone_supp">
+                            <label class="hide_input" for="photoPromotion">Changer la banniere</label>
+                            <input style="display:none;" type="file" id="photoPromotion" name="photoPromotion" accept=".png">
                             <label for="supp_image_promo">Supprimer la bannière</label>
                             <input type="checkbox" id="supp_image_promo" name="supp_image_promo">
                         </div>
