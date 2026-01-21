5a7ab201ff5143ade7a60cf6a5250d624c309caa
ytanneau <tannyann29@gmail.com>
2026-01-21 18:22:26 +0100

    update alarme

--- html/vendeur/alarme.php
+++ html/vendeur/alarme.php
@@ -9,6 +9,8 @@ define('HOME_SITE', '../');
 require_once HOME_GIT .".config.php";
 require_once(HOME_GIT . 'fonction_alarme.php');
 
+print_r($_SESSION);
+
 $data = get_alarme($_SESSION['raison_sociale']);
 
 print_r($data);
