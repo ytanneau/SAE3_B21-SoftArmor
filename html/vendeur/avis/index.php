1d61f2bb432b0ba5044bc2291a60ee5dea5c6de5
otterspace <thomastusaccin911@gmail.com>
2026-01-21 11:06:48 +0100

    Test affichage réponse

--- html/vendeur/avis/index.php
+++ html/vendeur/avis/index.php
@@ -138,6 +138,14 @@
                     $reponse = get_reponse($avis['id_avis']);
                     $image_pp = get_url_image($reponse['id_image_profil']);
 
+                    if (!$image_pp) {
+                        $image_pp = [
+                            "url" => "image/compte_vendeur.svg",
+                            "titre" => "Photo de profil",
+                            "alt" => "Photo de profil"
+                        ];
+                    }
+
                     // Si l'avis a une réponse, l'afficher
                     if (!empty($reponse)) { ?>
                         <li class="reponse">
