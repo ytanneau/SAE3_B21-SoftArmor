23767e0a05bc875502f18bf8338b71230e1173b7
titou_qlvr <titouan.quillevere@gmail.com>
2026-01-21 22:57:23 +0100

    aa okay

--- html/index.php
+++ html/index.php
@@ -121,8 +121,10 @@ if($tabPromotion != false){
         <!--Produit Banniere au jour (possiblement a faire)-->
         <?php if($pub){?>
         <div>
-            <h3>Mettre en avant</h3>
-            <p>Cette espace est dedié à la mise en avant d'un produit. Profitez-en au plus vite !!!</p>
+            <div class="banniere">
+                <h3>Mettre en avant</h3>
+                <p>Cette espace est dedié à la mise en avant d'un produit. Profitez-en au plus vite !!!</p>
+            </div>
         </div>
         <?php } else {?>
         <div class="banniere">
