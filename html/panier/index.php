c8541e2ffb27fc68a24e1d0fa83857288bd794e3
mathislqs <mathisloquais@gmail.com>
2026-01-21 16:42:57 +0100

    css footer

--- html/panier/index.php
+++ html/panier/index.php
@@ -95,7 +95,7 @@ if (isset($_SESSION['logged_in'])) {
         include HOME_SITE . "toolbar_categories.php";
     ?>
 
-    <main>
+    <main class = "mainPanier">
 
     <?php if (!$elts_panier) { ?>
         <div id="panier_vide">
