4f2c58403d244c269f8db15e8fabaf3c3563c329
ytanneau <tannyann29@gmail.com>
2026-01-21 18:35:20 +0100

    Update fonction_alarme.php

--- fonction_alarme.php
+++ fonction_alarme.php
@@ -2,7 +2,7 @@
 
     function get_alarme($id_vendeur){
         global $pdo;
-        $requete = $pdo->prepare("SELECT id_produit, nom_stock, quantite FROM produit_alerte WHERE id_vendeur = :id_vendeur");
+        $requete = $pdo->prepare("SELECT id_vendeur, id_produit, nom_stock, quantite FROM produit_alerte WHERE id_vendeur = :id_vendeur");
         
         $requete->bindValue(':id_vendeur', $id_vendeur, PDO::PARAM_INT);
         $requete->execute();
