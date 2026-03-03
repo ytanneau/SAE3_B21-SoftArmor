b93fd6563e7ca84fe6f87321203b85a89867aedb
titou_qlvr <titouan.quillevere@gmail.com>
2026-03-03 15:02:41 +0100

    okay on continu

--- html/recherche/interaction_carte.js
+++ html/recherche/interaction_carte.js
@@ -4,32 +4,32 @@ const illeetvilaine = document.getElementById("illeetvilaine")
 const morbihan = document.getElementById("morbihan")
 
 let polygonFinistere
+fetch("finistere.geojson").then(res => res.json()).then(data => {
+    polygonFinistere = L.geoJSON(data)
+})
 
-fetch("finistere.geojson")
-  .then(res => res.json())
-  .then(data => {
+let polygonCoteDarmor
+fetch("cotes-d-armor.geojson").then(res => res.json()).then(data => {
+    polygonCoteDarmor = L.geoJSON(data)
+})
 
-    polygonFinistere = L.geoJSON(data, {
-      style: {
-        color: "blue",
-        weight: 2,
-        fillOpacity: 0.2
-      }
-    })
-  })
 
 finistere.addEventListener('click', () => {
-
-  if (finistere.checked) {
-    polygonFinistere.addTo(map);
-    map.fitBounds(polygonFinistere.getBounds())
-  } else {
-    map.removeLayer(polygonFinistere)
-  }
-
+    if (finistere.checked) {
+        polygonFinistere.addTo(map)
+        map.fitBounds(polygonFinistere.getBounds())
+    } else {
+        map.removeLayer(polygonFinistere)
+        map.setView([48.113,-2.642],8)
+    }
 });
 
-
 cotedarmor.addEventListener('click', (e) => {
-    
+    if(cotedarmor.checked) {
+        polygonCoteDarmor.addTo(map)
+        map.fitBounds(polygonCoteDarmor.getBounds())
+    } else {
+        map.removeLayer(polygonCoteDarmor)
+        map.setView([48.113,-2.642],8)
+    }
 })
