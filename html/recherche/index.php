b63b02de05914258c1c0f5a0d2d588ab2b66bea6
mathislqs <mathisloquais@gmail.com>
2026-03-03 12:21:39 +0100

    Update index.php

--- html/recherche/index.php
+++ html/recherche/index.php
@@ -286,10 +286,10 @@ require_once (HOME_GIT . "fonction_vendeur.php");
                 price: {min: null, max: null},
                 sales: false,
                 reduc: false,
-                sellers: [
+                sellers: {
                     vendeur1 : "29",
                     vendeur2 : "30",
-                ]
+                }
             },
             sort: {
                 field: "nom_public", 
