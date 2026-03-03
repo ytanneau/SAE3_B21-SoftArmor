06bd531d605ad74c558ade60210880c2abc88ac4
titou_qlvr <titouan.quillevere@gmail.com>
2026-03-03 10:44:35 +0100

    voyons ??

--- html/recherche/index.php
+++ html/recherche/index.php
@@ -29,6 +29,9 @@ require_once (HOME_GIT . 'fonction_panier.php');
 require_once (HOME_GIT . 'fonction_categorie.php');
 require_once (HOME_GIT . 'fonction_recherche.php');
 
+    $tab_vendeurs = get_coor_id_vendeur();
+    $tab_adresse = get_adresse();
+
 ?>
 
 <!DOCTYPE html>
@@ -38,6 +41,13 @@ require_once (HOME_GIT . 'fonction_recherche.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include HOME_SITE . "link_head.php" ?>
     <title>Alizon - Recherche</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+    crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+    crossorigin=""></script>
 </head>
 <body data-page="search">
     <?php 
@@ -120,6 +130,33 @@ require_once (HOME_GIT . 'fonction_recherche.php');
         <section id="panneauCarte">
             <button id="btnFermerCarte">✕</button>
             <h2>Carte</h2>
+            <section>
+                <button id="btn_reset_filter">Supprimer les filtres</button>
+                <div id="tri_departement">
+                    <h2>Departement</h2>
+                    <div>
+                        <label for="cotedarmor">Cote d'armor - 22</label>
+                        <input type="checkbox" id="cotedarmor">
+                    </div>
+                    <div>
+                        <label for="finistere">Finistere - 29</label>
+                        <input type="checkbox" id="finistere">
+                    </div>
+                    <div>
+                        <label for="illeetvilaine">Ille et vilaine - 35</label>
+                        <input type="checkbox" id="illeetvilaine">
+                    </div>
+                    <div>
+                        <label for="morbihan">Morbihan - 56</label>
+                        <input type="checkbox" id="morbihan">
+                    </div>
+                </div>
+                <div id="liste_vendeur">
+                    <h2>Les vendeurs</h2>
+                    <div id="init"></div>
+                </div>
+            </section>
+            <div id="map"></div>
         </section>
         <script>
             document.addEventListener("DOMContentLoaded", () => {
@@ -144,8 +181,90 @@ require_once (HOME_GIT . 'fonction_recherche.php');
                 ombre.addEventListener("click", closePanel);
 
             });
-            
 
+            // AFFICHAGE DES FILTRES
+            const tab_vendeurs = <?= json_encode($tab_vendeurs)?>;
+            const tab_adresse = <?= json_encode($tab_adresse)?>;
+            const liste_vendeur = document.getElementById("liste_vendeur")
+            const init = document.getElementById("init")
+
+            function afficher_listes_vendeur(){
+                tab_vendeurs.forEach(vendeur => {
+                    let div = document.createElement("div")
+                    let input = document.createElement("input")
+                    input.type = "checkbox"
+                    let label = document.createElement("label")
+                    for(let cle in vendeur){
+                        if(cle == 'raison_sociale'){
+                            input.id = vendeur[cle]
+                            label.innerHTML = vendeur[cle]
+                            label.htmlFor = vendeur[cle]
+                        }
+                    }
+                    div.appendChild(label)
+                    div.appendChild(input)
+                    liste_vendeur.insertBefore(div, init)
+                });
+            }
+
+            afficher_listes_vendeur()
+
+            // GESTIONS DES FILTRES
+            const btn_reset_filter = document.getElementById("btn_reset_filter")
+            // const cotedarmor = document.getElementById("cotedarmor")
+            // const finistere = document.getElementById("finistere")
+            // const illeetvilaine = document.getElementById("illeetvilaine")
+            // const morbihan = document.getElementById("morbihan")
+            const input_checkbox = document.querySelectorAll("input[type=checkbox]")
+
+            console.log(input_checkbox)
+
+            btn_reset_filter.addEventListener('click', (e) => {
+                e.preventDefault()
+                input_checkbox.forEach(input => {
+                    input.checked = false
+                })
+            })  
+
+            // Initialisation de la carte
+            let map = L.map('map').setView([48.113,-2.642],8)
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map)
+
+            tab_vendeurs.forEach(vendeur => {
+                let tab_coor = []
+                let popup = L.popup()
+                let adresse = ""
+                let raison_sociale = ""
+                for(let info in vendeur){
+                    if(info == 'id_adresse'){
+                        tab_adresse.forEach(objet_adresse => {
+                            for(let cle in objet_adresse){
+                                if(cle == 'id_adresse'){
+                                    if(vendeur[info] == objet_adresse[cle]){
+                                        adresse = objet_adresse['adresse'] + 
+                                        objet_adresse['complement_adresse'] + ", " + 
+                                        objet_adresse['ville'] + ", " +
+                                        objet_adresse['code_postal']
+                                    }
+                                }
+                            }
+                        });
+                    }
+                    if((info == 'coor_x' || info == 'coor_y')&& vendeur[info] != null){
+                        tab_coor.push(vendeur[info])
+                    } else if (info == 'raison_sociale'){
+                        raison_sociale = vendeur[info]
+                    }
+                }
+                if(tab_coor.length == 2){
+                    let marker = L.marker(tab_coor).addTo(map)
+                    marker.bindPopup("<b>" + raison_sociale + "</b><br> Adresse : <br>" + adresse)
+                }
+            });
         </script>
     </main>
 
