0f799c2c1f7199b77b94ded1286f26d98dcf8acc
mathislqs <mathisloquais@gmail.com>
2026-03-03 12:08:57 +0100

    Update produits.php

--- html/recherche/produits.php
+++ html/recherche/produits.php
@@ -8,7 +8,7 @@
 
     // On récupère le corps de la requête HTTP (au format JSON) dans un tableau associatif
     $data = json_decode(file_get_contents('php://input'), true);
-    print_r($data);
+    // print_r($data);
     // On récupère la recherche, les filtres et tris éventuels
     $search     = $data['search'] ?? '';
     $filters    = $data['filters'] ?? [];
