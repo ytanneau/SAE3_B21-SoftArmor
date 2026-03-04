b106b9a8aefdbb7fd1bd1caa03a46664c5759e5c
titou_qlvr <titouan.quillevere@gmail.com>
2026-03-04 15:22:03 +0100

    src et non scr

--- html/vendeur/inscription/index.php
+++ html/vendeur/inscription/index.php
@@ -71,7 +71,7 @@
             </div>
             <input type="submit">
         </form>
-    <script scr="script_map.js"></script>
+    <script src="script_map.js"></script>
 <?php } elseif (isset($erreurs) && $erreurs == [] && $etape == 2) { ?>
 <main>
         <h1>Votre compte a été créé</h1>
