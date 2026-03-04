616783c170fb6e927d0ec0489a34a802332e780f
mathislqs <mathisloquais@gmail.com>
2026-03-04 12:11:30 +0100

    Update index.php

--- html/recherche/index.php
+++ html/recherche/index.php
@@ -328,9 +328,6 @@ require_once (HOME_GIT . "fonction_vendeur.php");
                 }
             });
         }
-        marker.addEventListener('change', (e) => {
-                        console.log("test marker");
-            });
         afficherMarker(listeVendeurAffiche)
     </script>
     <script type="text/javascript">
