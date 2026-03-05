02f94dd35c17201ab5a0c0360546ce1892c7c9d4
titou_qlvr <titouan.quillevere@gmail.com>
2026-03-05 13:44:29 +0100

    j'ai reusssisiiiiiii ouaiiiiiiiis

--- html/vendeur/inscription/script_map.js
+++ html/vendeur/inscription/script_map.js
@@ -9,5 +9,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const longitude = document.getElementById("longitude")
 const latitude = document.getElementById("latitude")
 
-let marker = L.marker([longitude.value, latitude.value])
+let marker = L.marker([longitude.value, latitude.value]).addTo(map)
 map.setView([longitude.value, latitude.value], 10)
