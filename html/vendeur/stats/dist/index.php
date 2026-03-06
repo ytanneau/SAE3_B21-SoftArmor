cacd3b30cef333901f7e1d5a64b079cb5dc6b889
ytanneau <tannyann29@gmail.com>
2026-03-06 11:01:24 +0100

    Update index.php

--- html/vendeur/stats/dist/index.php
+++ html/vendeur/stats/dist/index.php
@@ -8,6 +8,7 @@ define("HOME_SITE", "../../../");
   <head>
     <meta charset="UTF-8">
     <link rel="icon" href="/favicon.ico">
+    <?php include HOME_SITE . 'link_head.php'; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vite App</title>
     <script type="module" crossorigin src="./assets/index-q3jOH7mo.js"></script>
