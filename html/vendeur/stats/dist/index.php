c8d228ae8b66e047bd68a877fdade374c3812de3
ytanneau <tannyann29@gmail.com>
2026-03-06 11:22:00 +0100

    Update index.php

--- html/vendeur/stats/dist/index.php
+++ html/vendeur/stats/dist/index.php
@@ -13,8 +13,8 @@ session_start();
     <?php include HOME_SITE . 'link_head.php'; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vite App</title>
-    <script type="module" crossorigin src="/assets/index-DKmDw5fQ.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/index-gzs_Z5ZU.css">
+    <script type="module" crossorigin src="./assets/index-DKmDw5fQ.js"></script>
+    <link rel="stylesheet" crossorigin href="./assets/index-gzs_Z5ZU.css">
   </head>
   <body>
     <?php include HOME_SITE . "/vendeur/header.php" ?>
