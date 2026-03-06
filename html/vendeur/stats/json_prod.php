685f2b6cabaa99dd09183f105cdc2ab808de34ec
ytanneau <tannyann29@gmail.com>
2026-03-06 10:04:51 +0100

    update Graph

--- html/vendeur/stats/json_prod.php
+++ html/vendeur/stats/json_prod.php
@@ -1,10 +1,14 @@
 <?php
 
-        $sql= "SELECT * FROM stats_par_produit2 WHERE id_vendeur = :id_vendeur";
-        $requete = $pdo->prepare($sql);
-        $requete->bindValue(":id_vendeur", trim($$_SESSION['id_compte']), PDO::PARAM_INT);
-        $requete->execute();
-        $data= $requete->fetchAll(PDO::FETCH_ASSOC);
+define("HOME_GIT", "../../../");
+define("HOME_SITE", "../../");
+require_once HOME_GIT . ".config.php";
 
-        echo json_encode($data);
+$sql = "SELECT * FROM stats_par_produit2 WHERE id_vendeur = :id_vendeur";
+$requete = $pdo->prepare($sql);
+$requete->bindValue(":id_vendeur", trim($$_SESSION['id_compte']), PDO::PARAM_INT);
+$requete->execute();
+$data = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($data);
 ?>
