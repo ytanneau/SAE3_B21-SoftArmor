ad104078995e56cb5feb4142287cc7d2db3cce1c
ytanneau <tannyann29@gmail.com>
2026-03-06 11:17:50 +0100

    update css

--- html/vendeur/stats/dist/index.php
+++ html/vendeur/stats/dist/index.php
@@ -1,6 +1,8 @@
 <?php
 define("HOME_GIT", "../../../../");
 define("HOME_SITE", "../../../");
+
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -11,8 +13,8 @@ define("HOME_SITE", "../../../");
     <?php include HOME_SITE . 'link_head.php'; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vite App</title>
-    <script type="module" crossorigin src="./assets/index-q3jOH7mo.js"></script>
-    <link rel="stylesheet" crossorigin href="./assets/index-DAbL5Ljp.css">
+    <script type="module" crossorigin src="/assets/index-DKmDw5fQ.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/index-gzs_Z5ZU.css">
   </head>
   <body>
     <?php include HOME_SITE . "/vendeur/header.php" ?>
