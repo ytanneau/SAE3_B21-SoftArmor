fb3130962181551ec4737d07371ea77becc9f4d7
titou_qlvr <titouan.quillevere@gmail.com>
2026-03-10 11:20:53 +0100

    Okay lets go

--- html/vendeur/produit/modifier_promotion/index.php
+++ html/vendeur/produit/modifier_promotion/index.php
@@ -147,21 +147,21 @@
                 <p style="display:none;" id="warning1">Date de fin antérieur à la date de debut</p>
                 <p style="display:none;" id="warning2">Date(s) non selectionné(s)</p>
                 <p style="display:none;" id="warning4">Date de debut déjà passé</p>
+                <div class="block_banniere">
                 <?php if($tab_image_promotion != null){ ?>
-                    <div class="block_banniere">
                         <div class="overImg">
                             <label for="photoPromotion">Changer la banniere</label>
                             <input type="file" id="photoPromotion" name="photoPromotion" accept="image/png, image/webp, image/jpeg, image/jpg">
                         </div>
                         <img src=<?= HOME_SITE . $tab_image_promotion['url_image']?> alt="Banniere de promotion">
                         <a class="delete_picture" href="delete_photo.php?banniere=<?=$id_image_initial?>&promotion=<?=$id_promo?>&produit=<?=$id_produit?>">Supprimer la bannière</a>
-                    </div>
                 <?php } else { ?>
-                    <div class="ajout_banniere">
-                        <label class="hide_input_file" for="photoPromotion">Ajouter une bannière</label>
-                        <input style="display:none;" type="file" id="photoPromotion" name="photoPromotion" accept="image/png, image/webp, image/jpeg">
+                    <div class="overImg">
+                        <label for="photoPromotion">Ajouter une bannière</label>
+                        <input type="file" id="photoPromotion" name="photoPromotion" accept="image/png, image/webp, image/jpeg">
                     </div>
                 <?php } ?>
+                </div>
                 <input type="submit" id="valider" value="Valider">
                 <a 
                     id="supprimer_promotion"
