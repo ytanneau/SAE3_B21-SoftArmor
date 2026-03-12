c916aafadbd239bb7eed7b1f0f42537e7d6599cd
titou_qlvr <titouan.quillevere@gmail.com>
2026-03-12 09:27:45 +0100

    mais bordel fait attention

--- html/vendeur/produit/promotion/modifier_promotion/index.php
+++ html/vendeur/produit/promotion/modifier_promotion/index.php
@@ -121,7 +121,7 @@
         <link rel="stylesheet" href="<?=HOME_SITE?>style.css">
     </head>
     <body>
-        <?php include ".././../header.php" ?>
+        <?php include "../../../header.php" ?>
         <main class="main_promo">
             <div class="entete">
                 <a href="../index.php?produit=<?= $id_produit ?>"><img src="../../../../image/retour.svg" alt="bouton retour en arrière"></a>
