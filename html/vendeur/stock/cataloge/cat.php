fe0935c9d76ccc8959f14b43067c9e7886ee28f4
ytanneau <tannyann29@gmail.com>
2026-03-13 14:23:02 +0100

    Update cat.php

--- html/vendeur/stock/cataloge/cat.php
+++ html/vendeur/stock/cataloge/cat.php
@@ -46,15 +46,15 @@ class PDF extends tFPDF
         foreach ($data as $row) {
                 $x = $this->GetX();
                 $y = $this->GetY();
-                $this->Image(HOME_SITE . $row['url_image'], null,null,0, 30);
+                $this->Image(HOME_SITE . $row['url_image'], null,null,0, 20);
                 $this->SetXY(45, $y);
                 //$this->SetY($y);
-                $this->MultiCell(60, 15, $row['nom_public'], 0);
+                $this->MultiCell(60, 10, $row['nom_public'], 0);
                 $this->SetXY(50+65, $y);
-                $this->Cell(40, 30, round($row['prix'],2). " €", 0, 0, 'c');
+                $this->Cell(40, 20, round($row['prix'],2). " €", 0, 0, 'c');
             $this->Ln();
-            $this->Line(0, $y+30, 200, $y+31);
-            $this->SetY( $y+32);
+            $this->Line(0, $y+20, 200, $y+21);
+            $this->SetY( $y+22);
         }
         $i++;
         if($i == 8){
