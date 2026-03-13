e196f548034ced2f11c3ea01cb3843421e62291c
ytanneau <tannyann29@gmail.com>
2026-03-13 12:02:52 +0100

    Update cat.php

--- html/vendeur/stock/cataloge/cat.php
+++ html/vendeur/stock/cataloge/cat.php
@@ -28,6 +28,7 @@ $header = array('Pays', 'Capitale', 'Superficie (km²)', 'Pop. (milliers)');
 // Chargement des données
 //$data
 $pdf->SetFont('Arial', '', 14);
+$pdf->AddPage();
 $pdf->Text($pdf->GetX(),$pdf->GetY(), "Le prix est sujet a variation");
 $pdf->Ln();
 //$pdf->BasicTable($header, $data);
