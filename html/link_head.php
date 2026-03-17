c27268f179fa38ecb6e6f31d156473649a2fc4af
ytanneau <tannyann29@gmail.com>
2026-03-17 11:05:52 +0100

    Update link_head.php

--- html/link_head.php
+++ html/link_head.php
@@ -4,5 +4,5 @@
 
 <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
 <link rel="stylesheet" media="screen and (min-width: 1025px)" href="<?= HOME_SITE . "style.css" ?>">
-<link rel="stylesheet" media="screen and (max-width: 1024px)" href="<?= HOME_SITE ?> style_tele.css"/>
+<link rel="stylesheet" media="screen and (max-width: 1024px)" href="<?= HOME_SITE ?>style_tele.css"/>
 <link rel="icon" type="image/png" href="<?=HOME_SITE?>image/logo_Alizon_bleu.png">
