eca1de910feb9436ad302c54b022c85f7f31433b
titou_qlvr <titouan.quillevere@gmail.com>
2026-03-17 08:56:02 +0100

    aze

--- html/vendeur/produit/promotion/modifier_promotion/index.php
+++ html/vendeur/produit/promotion/modifier_promotion/index.php
@@ -160,7 +160,7 @@
                         <a class="delete_picture" href="delete_photo.php?banniere=<?=$id_image_initial?>&promotion=<?=$id_promo?>&produit=<?=$id_produit?>">Supprimer la bannière</a>
                 <?php } else { ?>
                     <div class="overImg">
-                        <label for="photoPromotion">Ajouter une bannière</label>
+                        <label for="photoPromotion">Ajouter une bannière (optionnel)</label>
                         <input type="file" id="photoPromotion" name="photoPromotion" accept="image/png, image/webp, image/jpeg">
                     </div>
                 <?php } ?>
